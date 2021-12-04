840f805233f0bc70fceda555347d9dec5e6f486f
Nihelkb <nihelkella@gmail.com>
2021-12-04 18:22:19 +0100

    Segundo problema

--- Dia01/SonarSweep.java
+++ Dia01/SonarSweep.java
@@ -7,27 +7,60 @@ public class SonarSweep{
         String line;
         int actualNum;
         int prevNum;
-        int counter = 0;
+        int incrCounter = 0;
         line = sc.nextLine();
         prevNum = Integer.parseInt(line);
         while(sc.hasNextLine()){
             line = sc.nextLine();
             actualNum = Integer.parseInt(line);
             if(actualNum > prevNum){
-                counter++;
+                incrCounter++;
             }
             prevNum = actualNum;
         }
         sc.close();
-        System.out.println(counter);
+        System.out.println(incrCounter);
     }
 
     private static void second() {
+        Scanner sc = new Scanner(System.in);
+        String line;
+        int actualNum;
+        int prevNum1 = 0;
+        int prevNum2 = 0;
+        int actualSum = 0;
+        int prevSum = 0;
+        int incrCounter = 0;
+        int elemCounter = 1;
+        while(elemCounter < 4 && sc.hasNextLine()){
+            line = sc.nextLine();
+            actualNum = Integer.parseInt(line);
+            prevSum += actualNum;
+            if(elemCounter == 2){
+                prevNum1 = actualNum;
+            }else if (elemCounter == 3){
+                prevNum2 = actualNum;
+            }
+            elemCounter++;
+        }
+        while(sc.hasNextLine()){
+            line = sc.nextLine();
+            actualNum = Integer.parseInt(line);
+            actualSum = actualNum + prevNum1 + prevNum2;
+            if(actualSum > prevSum){
+                incrCounter++;
+            }
+            prevNum1 = prevNum2;
+            prevNum2 = actualNum;
+            prevSum = actualSum;
+        }
+        sc.close();
+        System.out.println(incrCounter);
     }
 
     public static void main (String[] args){
         first();
-        //second();
+        second();
     }
     
 }
